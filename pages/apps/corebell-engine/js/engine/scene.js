9e596613eeb4ac092992a03d3473c67dc44110f0
rendani <rendaniman@outlook.com>
2025-01-18 20:11:33 +0200

    Update scene.js

--- pages/apps/corebell-engine/js/engine/scene.js
+++ pages/apps/corebell-engine/js/engine/scene.js
@@ -1,89 +1,77 @@
+
 // scene.js
 class Scene extends THREE.Scene {
     constructor() {
         super();
-        this.setupEnvironment();
+        this.objectsToUpdate = new Set();
+        this.setupBasicEnvironment();
     }
 
-    setupEnvironment() {
-        // Add ambient light
-        const ambientLight = new THREE.AmbientLight(0x404040);
-        this.add(ambientLight);
+    setupBasicEnvironment() {
+        // Simpler initial setup
+        this.add(new THREE.AmbientLight(0x404040));
+        
+        const light = new THREE.DirectionalLight(0xffffff, 1);
+        light.position.set(5, 5, 5);
+        this.add(light);
 
-        // Add directional light
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-        directionalLight.position.set(5, 5, 5);
-        this.add(directionalLight);
-
-        // Add grid helper
-        const gridHelper = new THREE.GridHelper(20, 20, 0x444444, 0x222222);
-        this.add(gridHelper);
-
-        // Add axis helper
-        const axisHelper = new THREE.AxesHelper(5);
-        this.add(axisHelper);
-
-        // Add environment fog
-        this.fog = new THREE.FogExp2(0x1a1a1a, 0.02);
+        // Minimal grid
+        this.add(new THREE.GridHelper(10, 10, 0x444444, 0x222222));
     }
 
     addCube(position = { x: 0, y: 0, z: 0 }) {
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshPhongMaterial({
-            color: Math.random() * 0xffffff,
-            shininess: 30,
-            metalness: 0.5,
-            roughness: 0.5
-        });
+        // Reuse geometry and material
+        if (!this.cubeGeometry) {
+            this.cubeGeometry = new THREE.BoxGeometry();
+            this.cubeMaterial = new THREE.MeshPhongMaterial({
+                color: 0x00ff9d,
+                shininess: 30
+            });
+        }
         
-        const cube = new THREE.Mesh(geometry, material);
+        const cube = new THREE.Mesh(this.cubeGeometry, this.cubeMaterial.clone());
         cube.position.set(position.x, position.y, position.z);
         cube.userData.type = 'cube';
-        cube.userData.rotationSpeed = {
-            x: Math.random() * 0.02,
-            y: Math.random() * 0.02,
-            z: Math.random() * 0.02
-        };
-        
         this.add(cube);
+        this.objectsToUpdate.add(cube);
         return cube;
     }
 
     addSphere(position = { x: 0, y: 0, z: 0 }) {
-        const geometry = new THREE.SphereGeometry(0.5, 32, 32);
-        const material = new THREE.MeshPhongMaterial({
-            color: Math.random() * 0xffffff,
-            shininess: 30,
-            metalness: 0.5,
-            roughness: 0.5
-        });
+        // Reuse geometry and material
+        if (!this.sphereGeometry) {
+            this.sphereGeometry = new THREE.SphereGeometry(0.5, 16, 16);
+            this.sphereMaterial = new THREE.MeshPhongMaterial({
+                color: 0x00ff9d,
+                shininess: 30
+            });
+        }
         
-        const sphere = new THREE.Mesh(geometry, material);
+        const sphere = new THREE.Mesh(this.sphereGeometry, this.sphereMaterial.clone());
         sphere.position.set(position.x, position.y, position.z);
         sphere.userData.type = 'sphere';
-        sphere.userData.rotationSpeed = {
-            x: Math.random() * 0.02,
-            y: Math.random() * 0.02,
-            z: Math.random() * 0.02
-        };
-        
         this.add(sphere);
+        this.objectsToUpdate.add(sphere);
         return sphere;
     }
 
     updateObjects(delta) {
-        this.traverse((object) => {
-            if (object.isMesh && object.userData.rotationSpeed) {
-                object.rotation.x += object.userData.rotationSpeed.x;
-                object.rotation.y += object.userData.rotationSpeed.y;
-                object.rotation.z += object.userData.rotationSpeed.z;
-            }
-        });
+        for (const object of this.objectsToUpdate) {
+            object.rotation.x += delta * 0.5;
+            object.rotation.y += delta * 0.5;
+        }
     }
 
-    removeObject(object) {
-        this.remove(object);
-        if (object.geometry) object.geometry.dispose();
-        if (object.material) object.material.dispose();
+    dispose() {
+        this.traverse((object) => {
+            if (object.geometry) object.geometry.dispose();
+            if (object.material) {
+                if (Array.isArray(object.material)) {
+                    object.material.forEach(material => material.dispose());
+                } else {
+                    object.material.dispose();
+                }
+            }
+        });
     }
 }
